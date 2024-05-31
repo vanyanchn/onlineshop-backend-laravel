66511960d944be8a209f7c354da6a15d6d6c7264
HakimIqbal <muh.iqbal.hakim@gmail.com>
2024-05-31 11:06:43 +0700

    API login, Logout, Register

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // register
 Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+//logout
+Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+//login
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
